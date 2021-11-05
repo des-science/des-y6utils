af4e0a2c53b676cb0e70cec0312da0c349508b5a
Matthew R. Becker <becker.mr@gmail.com>
2021-11-05 16:57:28 -0500

    ENH code to make the star grids and check them

--- des_y6utils/piff/__init__.py
+++ des_y6utils/piff/__init__.py
@@ -1,1 +1,9 @@
-from .flag_good_regions import make_good_regions_for_piff_model  # noqa
+# flake8: noqa
+from .flag_good_regions import (
+    make_good_regions_for_piff_model_gal_grid,
+    make_good_regions_for_piff_model_star_and_gal_grid,
+    nanmad,
+    measure_t_grid_for_piff_model,
+    measure_star_t_for_piff_model,
+    map_star_t_to_grid,
+)
--- des_y6utils/piff/flag_good_regions.py
+++ des_y6utils/piff/flag_good_regions.py
@@ -3,6 +3,9 @@ from ngmix.admom import AdmomFitter
 import galsim
 import copy
 import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LinearRegression
 
 ALL_BAD = 2**0
 BAD_BOX = 2**1
@@ -94,7 +97,7 @@ def _make_a_good_box(bad_msk, verbose=False):
     return b, flag
 
 
-def _nanmad(x, axis=None):
+def nanmad(x, axis=None):
     """
     median absolute deviation - scaled like a standard deviation
 
@@ -123,7 +126,223 @@ def _rescale_b(b, grid_size):
     return b
 
 
-def make_good_regions_for_piff_model(
+def measure_t_grid_for_piff_model(
+    piff_mod, piff_kwargs, grid_size=128, seed=None
+):
+    """Make a bounding box of good regions for a given Piff model.
+
+    Parameters
+    ----------
+    piff_mod : piff Model
+        The Piff model read via `piff.read`.
+    piff_kwargs : dict, optional
+        Any extra keyword arguments to pass to `piff_mod.draw`. Typically these
+        might include things like `{"GI_COLOR": 0.61}`.
+    grid_size : int, optional
+        The grid size to construct the map. Must divide 2048 and 4096 evenly.
+        Default is 128.
+    seed : int, optional
+        The seed to use for the RNG fed to ngmix. Default of None will seed the
+        RNG on-the-fly.
+
+    Returns
+    -------
+    t_arr : array
+        The map of T over the model with one cell per `grid_size`.
+    """
+    piff_kwargs = piff_kwargs or {}
+    rng = np.random.RandomState(seed=seed)
+
+    if (
+        ((4096//grid_size) * grid_size != 4096)
+        or ((2048//grid_size) * grid_size != 2048)
+    ):
+        raise RuntimeError("The grid size must evenly divide 4096 and 2048.")
+
+    delta_to_pix = np.floor((grid_size-1)/2 + 0.5)
+    y = np.arange(4096//grid_size)*grid_size + delta_to_pix
+    x = np.arange(2048//grid_size)*grid_size + delta_to_pix
+
+    t_arr = np.zeros((y.shape[0], x.shape[0])) + np.nan
+    for i, yv in enumerate(y):
+        for j, xv in enumerate(x):
+            img = piff_mod.draw(
+                x=xv+1, y=yv+1, chipnum=list(piff_mod.wcs.keys())[0],
+                **piff_kwargs,
+            )
+            obs = ngmix.Observation(
+                image=img.array,
+                jacobian=ngmix.Jacobian(
+                    y=img.center.y - img.bounds.ymin,
+                    x=img.center.x - img.bounds.xmin,
+                    wcs=img.wcs.local(
+                        image_pos=galsim.PositionD(x=xv+1, y=yv+1)
+                    ).jacobian(),
+                )
+            )
+            try:
+                res = AdmomFitter(
+                    rng=rng
+                ).go(obs, ngmix.moments.fwhm_to_T(1))
+                if res["flags"] == 0:
+                    t_arr[i, j] = res["T"]
+            except Exception:
+                pass
+
+    return t_arr
+
+
+def _get_star_stamp_pos(s, img, wgt):
+    xint = int(np.floor(s.x - 1 + 0.5))
+    yint = int(np.floor(s.y - 1 + 0.5))
+    bbox = 17
+    bbox_2 = (bbox - 1)//2
+
+    return dict(
+        img=img[yint-bbox_2: yint+bbox_2+1, xint-bbox_2: xint+bbox_2+1],
+        wgt=wgt[yint-bbox_2: yint+bbox_2+1, xint-bbox_2: xint+bbox_2+1],
+        xstart=xint-bbox_2,
+        ystart=yint-bbox_2,
+        dim=bbox,
+        x=s.x - 1,
+        y=s.y - 1,
+    )
+
+
+def _get_star_piff_obs(piff_mod, s, img, wgt, piff_prop):
+
+    if piff_prop:
+        kwargs = {
+            piff_prop: s.data.properties[piff_prop]
+        }
+    else:
+        kwargs = {}
+    sres = _get_star_stamp_pos(s, img, wgt)
+
+    xv = sres["x"]+1
+    yv = sres["y"]+1
+    wcs = list(piff_mod.wcs.values())[0].local(
+        image_pos=galsim.PositionD(x=xv, y=yv)
+    ).jacobian()
+    img = galsim.ImageD(sres["dim"], sres["dim"], wcs=wcs)
+    cen = (
+        sres["x"] - sres["xstart"] + 1,
+        sres["y"] - sres["ystart"] + 1,
+    )
+    img = piff_mod.draw(
+        x=xv, y=yv, chipnum=list(piff_mod.wcs.keys())[0],
+        image=img, center=cen, **kwargs,
+    )
+    model_obs = ngmix.Observation(
+        image=img.array,
+        jacobian=ngmix.Jacobian(
+            y=cen[1]-1,
+            x=cen[0]-1,
+            wcs=wcs,
+        )
+    )
+    star_obs = ngmix.Observation(
+        image=sres["img"],
+        weight=sres["wgt"],
+        jacobian=ngmix.Jacobian(
+            y=cen[1]-1,
+            x=cen[0]-1,
+            wcs=wcs,
+        )
+    )
+    return model_obs, star_obs, sres
+
+
+def measure_star_t_for_piff_model(piff_mod, img, wgt, piff_prop=None, seed=None):
+    """Make a bounding box of good regions for a given Piff model.
+
+    Parameters
+    ----------
+    piff_mod : piff Model
+        The Piff model read via `piff.read`.
+    img : array
+        The image.
+    wgt : array
+        The weight
+    piff_prop : str, optional
+        The name of the piff property to use. Should be one of "GI_COLOR" or "IZ_COLOR"
+        for Y6.
+    seed : int, optional
+        The seed to use for the RNG fed to ngmix. Default of None will seed the
+        RNG on-the-fly.
+
+    Returns
+    -------
+    data : array
+        An array with columns x, y, t for the stars.
+    """
+    piff_prop = piff_prop or {}
+    rng = np.random.RandomState(seed=seed)
+
+    nd = len(piff_mod.stars)
+    data = np.zeros(nd, dtype=[("x", "f8"), ("y", "f8"), ("t", "f8")])
+    for col in data.dtype.names:
+        data[col] += np.nan
+
+    for i, s in enumerate(piff_mod.stars):
+
+        mobs, sobs, sres = _get_star_piff_obs(piff_mod, s, img, wgt, piff_prop)
+
+        try:
+            res = ngmix.admom.AdmomFitter(
+                rng=rng
+            ).go(mobs, ngmix.moments.fwhm_to_T(1))
+            if res["flags"] == 0:
+                data["x"][i] = sres["x"]
+                data["y"][i] = sres["y"]
+                data["t"][i] = res["T"]
+        except Exception:
+            pass
+
+    return data
+
+
+def map_star_t_to_grid(star_data, grid_size=128, degree=2):
+    """Fit star T data to a polynomial and evaluate on a grid.
+
+    Parameters
+    ----------
+    star_data : array
+        The array pf star data from `measure_star_t_for_piff_model`.
+    grid_size : int, optional
+        The grid size to construct the map. Must divide 2048 and 4096 evenly.
+        Default is 128.
+    degree : int, optional
+        The degree of the polynomial.
+
+    Returns
+    -------
+    tg : array
+        The array of T values from evaluating the git on a grid.
+    """
+    # see this blog post
+    # https://towardsdatascience.com/polynomial-regression-with-scikit-learn-what-you-should-know-bed9d3296f2
+    polyreg = make_pipeline(PolynomialFeatures(degree), LinearRegression())
+    polyreg.fit(
+        np.array([star_data["x"], star_data["y"]]).T,
+        np.array(star_data["t"]),
+    )
+
+    if (
+        ((4096//grid_size) * grid_size != 4096)
+        or ((2048//grid_size) * grid_size != 2048)
+    ):
+        raise RuntimeError("The grid size must evenly divide 4096 and 2048.")
+
+    delta_to_pix = np.floor((grid_size-1)/2 + 0.5)
+    y, x = np.mgrid[0:4096:grid_size, 0:2048:grid_size] + delta_to_pix
+    tg = polyreg.predict(np.array([x.ravel(), y.ravel()]).T)
+    tg = tg.reshape(x.shape)
+
+    return tg
+
+
+def make_good_regions_for_piff_model_gal_grid(
     piff_mod, piff_kwargs=None, grid_size=128, seed=None, verbose=False,
     any_bad_thresh=25, flag_bad_thresh=15,
 ):
@@ -173,44 +392,10 @@ def make_good_regions_for_piff_model(
                 A dictionary with keys xmin, xmax, ymin, and ymax. Any model
                 whose center falls within this box is considered OK.
     """
-    piff_kwargs = piff_kwargs or {}
-    rng = np.random.RandomState(seed=seed)
+    t_arr = measure_t_grid_for_piff_model(
+        piff_mod, piff_kwargs, grid_size=grid_size, seed=seed
+    )
     flags = 0
-
-    if (
-        ((4096//grid_size) * grid_size != 4096)
-        or ((2048//grid_size) * grid_size != 2048)
-    ):
-        raise RuntimeError("The grid size must evenly divide 4096 and 2048.")
-
-    delta_to_pix = np.floor((grid_size-1)/2 + 0.5)
-    y = np.arange(4096//grid_size)*grid_size + delta_to_pix
-    x = np.arange(2048//grid_size)*grid_size + delta_to_pix
-
-    t_arr = np.zeros((y.shape[0], x.shape[0])) + np.nan
-    for i, yv in enumerate(y):
-        for j, xv in enumerate(x):
-            img = piff_mod.draw(
-                x=xv, y=yv, chipnum=list(piff_mod.wcs.keys())[0],
-                **piff_kwargs,
-            )
-            obs = ngmix.Observation(
-                image=img.array,
-                jacobian=ngmix.Jacobian(
-                    y=img.center.y - img.bounds.ymin,
-                    x=img.center.x - img.bounds.xmin,
-                    wcs=img.wcs.local(
-                        image_pos=galsim.PositionD(x=xv, y=yv)
-                    ).jacobian(),
-                )
-            )
-            try:
-                res = AdmomFitter(
-                    rng=rng
-                ).go(obs, ngmix.moments.fwhm_to_T(1))
-                t_arr[i, j] = res["T"]
-            except Exception:
-                pass
 
     b = dict(
         xmax=t_arr.shape[1]-1,
@@ -226,7 +411,7 @@ def make_good_regions_for_piff_model(
 
     if flags == 0:
         t_mn = np.nanmedian(t_arr)
-        t_std = _nanmad(t_arr)
+        t_std = nanmad(t_arr)
 
         any_very_bad = (
             (~np.isfinite(t_arr))
@@ -245,5 +430,115 @@ def make_good_regions_for_piff_model(
         t_mn=t_mn,
         t_std=t_std,
         t_arr=t_arr,
+        bbox=_rescale_b(b, grid_size),
+    )
+
+
+def make_good_regions_for_piff_model_star_and_gal_grid(
+    piff_mod, img, wgt, piff_kwargs=None, grid_size=128, seed=None, verbose=False,
+    any_bad_thresh=5, flag_bad_thresh=5, degree=2,
+):
+    """Make a bounding box of good regions for a given Piff model.
+
+    Parameters
+    ----------
+    piff_mod : piff Model
+        The Piff model read via `piff.read`.
+    img : array
+        The image.
+    wgt : array
+        The weight.
+    piff_kwargs : dict, optional
+        Any extra keyword arguments to pass to `piff_mod.draw`. Typically these
+        might include things like `{"GI_COLOR": 0.61}`.
+    grid_size : int, optional
+        The grid size to construct the map. Must divide 2048 and 4096 evenly.
+        Default is 128.
+    seed : int, optional
+        The seed to use for the RNG fed to ngmix. Default of None will seed the
+        RNG on-the-fly.
+    verbose : bool, optional
+        If True, print some stats as the code is running. Default of False makes
+        the code silent.
+    any_bad_thresh : float, optional
+        The threshold used to figure out if any region of the CCD is bad. Models
+        with any regions where |t - t_mn| > any_bad_thresh * t_std are considered
+        bad. The default of 25 appears to account for PSF size variation while also
+        flagging weird models.
+    flag_bad_thresh : float, optional
+        The threshold used to mark good versus bad regions. Any region of the model
+        where |t - t_mn| > any_bad_thresh * t_std is marked as bad if any
+        region also exceeds the `any_bad_thresh`. Default of 15 accounts for
+        model size variation while also avoiding the very bad regions.
+    degree : int, optional
+        The degree of the polynomial.
+
+    Returns
+    -------
+    data : dict
+        A dictionary with the following keys:
+
+            flags : int
+                The flags value. If non-zero, the entire model is flagged.
+            t_star : float
+                The star T map.
+            t_gal : float
+                The gal T map.
+            bbox : dict
+                A dictionary with keys xmin, xmax, ymin, and ymax. Any model
+                whose center falls within this box is considered OK.
+    """
+    flags = 0
+
+    tg_arr = measure_t_grid_for_piff_model(
+        piff_mod, piff_kwargs, grid_size=grid_size, seed=seed
+    )
+    data = measure_star_t_for_piff_model(
+        piff_mod, img, wgt, piff_prop=list(piff_kwargs.keys())[0], seed=seed,
+    )
+
+    if np.all(np.isnan(data["t"])):
+        flags |= ALL_BAD
+
+    if flags == 0:
+        msk = np.isfinite(data["t"])
+
+        ts_arr = map_star_t_to_grid(
+            data[msk], grid_size=grid_size, degree=degree,
+        )
+    else:
+        ts_arr = None
+
+    b = dict(
+        xmax=tg_arr.shape[1]-1,
+        xmin=0,
+        ymax=tg_arr.shape[0]-1,
+        ymin=0,
+    )
+
+    if flags == 0 and (not np.any(np.isfinite(tg_arr))) or np.any(np.isnan(ts_arr)):
+        flags |= ALL_BAD
+
+    if flags == 0:
+        tdiff = tg_arr - ts_arr
+        t_mn = np.nanmedian(tdiff)
+        t_std = nanmad(tdiff)
+
+        any_very_bad = (
+            (~np.isfinite(tdiff))
+            | (np.abs(tdiff - t_mn) > any_bad_thresh * t_std)
+        )
+        if np.any(any_very_bad):
+            some_bad = (
+                (~np.isfinite(tg_arr))
+                | (np.abs(tdiff - t_mn) > flag_bad_thresh * t_std)
+            )
+            b, flag = _make_a_good_box(some_bad, verbose=verbose)
+            flags |= flag
+
+    return dict(
+        flags=flags,
+        t_star=ts_arr,
+        t_gal=tg_arr,
         bbox=_rescale_b(b, grid_size),
     )
