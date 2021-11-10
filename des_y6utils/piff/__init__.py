00417c41fc239981ed07df91ca3e8b4ff3c99433
Matthew R. Becker <becker.mr@gmail.com>
2021-11-09 21:21:44 -0600

    BUG set version

--- des_y6utils/piff/__init__.py
+++ des_y6utils/piff/__init__.py
@@ -1,4 +1,15 @@
 # flake8: noqa
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("des_y6utils")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
 from .flag_good_regions import (
     make_good_regions_for_piff_model_gal_grid,
     make_good_regions_for_piff_model_star_and_gal_grid,
