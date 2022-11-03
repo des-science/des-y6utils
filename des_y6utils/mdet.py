0ccb8c9e7d5b0112b88b4c6c9dda2a28a37e3b9c
beckermr <becker.mr@gmail.com>
2022-11-03 15:16:20 -0500

    ENH add random cut function

--- des_y6utils/mdet.py
+++ des_y6utils/mdet.py
@@ -38,7 +38,195 @@ def make_mdet_cuts(data, version, verbose=False):
         raise ValueError("the mdet cut version '%r' is not recognized!" % version)
 
 
-def _make_mdet_cuts_raw_v12(d, verbose=False):
+def _make_mdet_cuts(
+    data,
+    verbose=False,
+    min_s2n=10,
+    n_terr=3,
+    min_t_ratio=1.2,
+    max_mfrac=0.1,
+    max_s2n=np.inf,
+    mask_name="y6-combined-hleda-gaiafull-des-stars-hsmap16384-nomdet-v2.fits"
+):
+    """
+    Returns
+    -------
+    msk : np.ndarray of bool
+        A boolean array with the cuts. To cut the data, use `data[msk]`.
+    """
+    msk = _make_mdet_cuts_raw_v12(
+        data,
+        verbose=verbose,
+        min_s2n=min_s2n,
+        n_terr=n_terr,
+        min_t_ratio=min_t_ratio,
+        max_mfrac=max_mfrac,
+        max_s2n=max_s2n,
+    )
+
+    # apply the mask
+    if os.path.exists(mask_name):
+        hmap = _read_hsp_mask_local(mask_name)
+    else:
+        hmap = _read_hsp_mask(mask_name)
+    in_footprint = hmap.get_values_pos(
+        data["ra"], data["dec"], valid_mask=True
+    )
+    msk &= in_footprint
+    if verbose:
+        print("did mask cuts", np.sum(msk))
+
+    return msk
+
+
+def _make_mdet_cuts_v1(d, verbose=False):
+
+    msk = _make_mdet_cuts_raw_v12(
+        d,
+        verbose=verbose,
+        min_s2n=10,
+        n_terr=3,
+        min_t_ratio=1.2,
+        max_mfrac=0.1,
+        max_s2n=np.inf,
+    )
+
+    # apply the mask
+    hmap = _read_hsp_mask("y6-combined-hleda-gaiafull-hsmap16384-nomdet.fits")
+    in_footprint = hmap.get_values_pos(d["ra"], d["dec"], valid_mask=True)
+    msk &= in_footprint
+    if verbose:
+        print("did mask cuts", np.sum(msk))
+
+    return msk
+
+
+def _make_mdet_cuts_v2(d, verbose=False):
+
+    msk = _make_mdet_cuts_raw_v12(
+        d,
+        verbose=verbose,
+        min_s2n=10,
+        n_terr=3,
+        min_t_ratio=1.2,
+        max_mfrac=0.1,
+        max_s2n=np.inf,
+    )
+
+    # apply the mask
+    hmap = _read_hsp_mask(
+        "y6-combined-hleda-gaiafull-des-stars-hsmap16384-nomdet-v2.fits"
+    )
+    in_footprint = hmap.get_values_pos(d["ra"], d["dec"], valid_mask=True)
+    msk &= in_footprint
+    if verbose:
+        print("did mask cuts", np.sum(msk))
+
+    return msk
+
+
+def _compute_asinh_mags(flux, i):
+    """This function and coefficients are from from Eli. Ask him.
+
+    Parameters
+    ----------
+    flux : float or np.ndarray
+        The flux.
+    i : int
+        The index of the band in griz (i.e., 0 for g, 1 for r, 2 for i, 3 for z).
+
+    Returns
+    -------
+    mag : float or np.ndarray
+        The asinh magnitude for the flux.
+    """
+    zp = 30.0
+    # array is griz
+    b_array = np.array([3.27e-12, 4.83e-12, 6.0e-12, 9.0e-12])
+    bscale = np.array(b_array) * 10.**(zp / 2.5)
+    mag = (
+        2.5 * np.log10(1.0 / b_array[i])
+        - np.arcsinh(0.5 * flux / bscale[i]) / (0.4 * np.log(10.0))
+    )
+    # mag_err = (
+    #     2.5 * fluxerr / (
+    #         2.0 * bscale[i] * np.log(10.0)
+    #         * np.sqrt(1.0 + (0.5 * flux / bscale[i])**2.)
+    #     )
+    # )
+    return mag
+
+
+@lru_cache
+def _read_hsp_mask(fname):
+    mpth = _get_mask_path(fname)
+    return healsparse.HealSparseMap.read(mpth)
+
+
+@lru_cache
+def _read_hsp_mask_local(fname):
+    return healsparse.HealSparseMap.read(fname)
+
+
+def _get_mask_path(fname):
+    # get or make meds dir
+    meds_dir = os.environ.get("MEDS_DIR", None)
+    if meds_dir is None:
+        meds_dir = os.path.expandvars(
+            os.path.expanduser("~/MEDS_DIR")
+        )
+        os.makedirs(meds_dir, exist_ok=True)
+
+    # download if needed
+    fpth = os.path.join(meds_dir, fname)
+    if not os.path.exists(fpth):
+        _download_fname_from_bnl(fpth)
+
+    return fpth
+
+
+def _download_fname_from_bnl(fpth):
+    fdir, fname = os.path.split(fpth)
+
+    wget_res = subprocess.run("which wget", shell=True, capture_output=True)
+    curl_res = subprocess.run("which curl", shell=True, capture_output=True)
+
+    bnl = "https://www.cosmo.bnl.gov/www/esheldon/data/y6-healsparse"
+    if wget_res.returncode == 0:
+        subprocess.run(
+            "cd %s && wget %s/%s" % (
+                fdir, bnl, fname,
+            ),
+            shell=True,
+            check=True,
+            capture_output=True,
+        )
+    elif curl_res.returncode == 0:
+        subprocess.run(
+            "cd %s && curl -L %s/%s --output %s" % (
+                fdir, bnl, fname, fname,
+            ),
+            shell=True,
+            check=True,
+            capture_output=True,
+        )
+    else:
+        raise RuntimeError(
+            "Could not download mask '%s' from BNL due "
+            "to wget or curl missing!" % fname,
+        )
+
+
+def _make_mdet_cuts_raw_v12(
+    d,
+    *,
+    min_s2n,
+    n_terr,
+    min_t_ratio,
+    max_mfrac,
+    max_s2n,
+    verbose=False,
+):
     """The raw v1 cuts come from extensive analysis over summer 2022. They
     reflect a first-pass at a consensus set of cuts.
 
@@ -95,8 +283,9 @@ def _make_mdet_cuts_raw_v12(d, verbose=False):
 
     # now do the rest
     msk &= (
-        (d["wmom_s2n"] > 10)
-        & (d["mfrac"] < 0.1)
+        (d["wmom_s2n"] > min_s2n)
+        & (d["wmom_s2n"] < max_s2n)
+        & (d["mfrac"] < max_mfrac)
         & (np.abs(gmr) < 5)
         & (np.abs(rmi) < 5)
         & (np.abs(imz) < 5)
@@ -111,8 +300,8 @@ def _make_mdet_cuts_raw_v12(d, verbose=False):
         & (d["pgauss_T"] < (1.9 - 2.8*d["pgauss_T_err"]))
         & (
             d["wmom_T_ratio"] >= np.maximum(
-                1.2,
-                (1.0 + 3.0*d["wmom_T_err"]/d["wmom_psf_T"])
+                min_t_ratio,
+                (1.0 + n_terr*d["wmom_T_err"]/d["wmom_psf_T"])
             )
         )
     )
@@ -120,120 +309,3 @@ def _make_mdet_cuts_raw_v12(d, verbose=False):
         print("did mdet cuts", np.sum(msk))
 
     return msk
-
-
-def _make_mdet_cuts_v1(d, verbose=False):
-
-    msk = _make_mdet_cuts_raw_v12(d, verbose=verbose)
-
-    # apply the mask
-    hmap = _read_hsp_mask("y6-combined-hleda-gaiafull-hsmap16384-nomdet.fits")
-    in_footprint = hmap.get_values_pos(d["ra"], d["dec"], valid_mask=True)
-    msk &= in_footprint
-    if verbose:
-        print("did mask cuts", np.sum(msk))
-
-    return msk
-
-
-def _make_mdet_cuts_v2(d, verbose=False):
-
-    msk = _make_mdet_cuts_raw_v12(d, verbose=verbose)
-
-    # apply the mask
-    hmap = _read_hsp_mask(
-        "y6-combined-hleda-gaiafull-des-stars-hsmap16384-nomdet-v2.fits"
-    )
-    in_footprint = hmap.get_values_pos(d["ra"], d["dec"], valid_mask=True)
-    msk &= in_footprint
-    if verbose:
-        print("did mask cuts", np.sum(msk))
-
-    return msk
-
-
-def _compute_asinh_mags(flux, i):
-    """This function and coefficients are from from Eli. Ask him.
-
-    Parameters
-    ----------
-    flux : float or np.ndarray
-        The flux.
-    i : int
-        The index of the band in griz (i.e., 0 for g, 1 for r, 2 for i, 3 for z).
-
-    Returns
-    -------
-    mag : float or np.ndarray
-        The asinh magnitude for the flux.
-    """
-    zp = 30.0
-    # array is griz
-    b_array = np.array([3.27e-12, 4.83e-12, 6.0e-12, 9.0e-12])
-    bscale = np.array(b_array) * 10.**(zp / 2.5)
-    mag = (
-        2.5 * np.log10(1.0 / b_array[i])
-        - np.arcsinh(0.5 * flux / bscale[i]) / (0.4 * np.log(10.0))
-    )
-    # mag_err = (
-    #     2.5 * fluxerr / (
-    #         2.0 * bscale[i] * np.log(10.0)
-    #         * np.sqrt(1.0 + (0.5 * flux / bscale[i])**2.)
-    #     )
-    # )
-    return mag
-
-
-@lru_cache
-def _read_hsp_mask(fname):
-    mpth = _get_mask_path(fname)
-    return healsparse.HealSparseMap.read(mpth)
-
-
-def _get_mask_path(fname):
-    # get or make meds dir
-    meds_dir = os.environ.get("MEDS_DIR", None)
-    if meds_dir is None:
-        meds_dir = os.path.expandvars(
-            os.path.expanduser("~/MEDS_DIR")
-        )
-        os.makedirs(meds_dir, exist_ok=True)
-
-    # download if needed
-    fpth = os.path.join(meds_dir, fname)
-    if not os.path.exists(fpth):
-        _download_fname_from_bnl(fpth)
-
-    return fpth
-
-
-def _download_fname_from_bnl(fpth):
-    fdir, fname = os.path.split(fpth)
-
-    wget_res = subprocess.run("which wget", shell=True, capture_output=True)
-    curl_res = subprocess.run("which curl", shell=True, capture_output=True)
-
-    bnl = "https://www.cosmo.bnl.gov/www/esheldon/data/y6-healsparse"
-    if wget_res.returncode == 0:
-        subprocess.run(
-            "cd %s && wget %s/%s" % (
-                fdir, bnl, fname,
-            ),
-            shell=True,
-            check=True,
-            capture_output=True,
-        )
-    elif curl_res.returncode == 0:
-        subprocess.run(
-            "cd %s && curl -L %s/%s --output %s" % (
-                fdir, bnl, fname, fname,
-            ),
-            shell=True,
-            check=True,
-            capture_output=True,
-        )
-    else:
-        raise RuntimeError(
-            "Could not download mask '%s' from BNL due "
-            "to wget or curl missing!" % fname,
-        )
