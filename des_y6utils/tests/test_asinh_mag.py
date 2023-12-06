241cf63198924df447f9fba54be9155b91ed7971
Masaya Yamamoto <masaya.yamamoto@duke.edu>
2023-12-06 07:37:03 -0800

    fixed unit tests

--- des_y6utils/tests/test_asinh_mag.py
+++ des_y6utils/tests/test_asinh_mag.py
@@ -1,4 +1,5 @@
 from ..mdet import _compute_asinh_dered_mag, _compute_asinh_flux, _compute_asinh_mags
+import math
 
 
 def test_compute_asinh_flux():
@@ -7,7 +8,7 @@ def test_compute_asinh_flux():
     mag_g = _compute_asinh_mags(flux_input, 0)
     flux_g = _compute_asinh_flux(mag_g, 0)
 
-    assert flux_input == flux_g
+    assert math.isclose(flux_input, flux_g, rel_tol=1e-9)
 
 
 def test_compute_asinh_dered_mag():
@@ -15,4 +16,4 @@ def test_compute_asinh_dered_mag():
     mag_g = _compute_asinh_mags(flux_input, 0)
     dered_mag_g = _compute_asinh_dered_mag(flux_input, 0, 1 / 3.186)
 
-    assert mag_g == dered_mag_g
+    assert math.isclose(mag_g - 1.0, dered_mag_g, rel_tol=1e-9)
